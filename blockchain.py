c6a8f126964fbd31fcd8b6ba7b7b2ec49f7fe87c
Francis Yang <xyang42@ucsc.edu>
2017-10-02 19:14:57 -0700

    Added new transaction

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,21 @@
+# Responsible for managing the chain.
+# It will store transactions and have some helper methods for adding new blocks to the chain.
+# Here is an example of what a single Block looks like:
+#
+# block = {
+#     'index': 1,
+#     'timestamp': 1506057125.900785,
+#     'transactions': [
+#         {
+#             'sender': "8527147fe1f5426f9dd545de4b27ee00",
+#             'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+#             'amount': 5,
+#         }
+#     ],
+#     'proof': 324984774000,
+#     'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+# }
+
 class Blockchain(object):
     def _init_(self):
         self.chain = []
@@ -8,8 +26,18 @@ class Blockchain(object):
         pass
 
     # Add a new transaction to the list of transactions
-    def new_transaction(self):
-        pass
+    # Creates a new transaction to go into the next mined block.
+    # sender: <str> Address of the sender.
+    # recipient: <str> Address of the recipient.
+    # amount: <int> Amount.
+    # return: <int> The index of the block which the transaction will be added to - the next one to be mined.
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+        return self.last_block['index'] + 1
 
     # Hash a block
     @staticmethod
