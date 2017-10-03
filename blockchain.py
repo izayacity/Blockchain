28c47b889defffbc7dc4761263347a88ee6439c0
Francis Yang <xyang42@ucsc.edu>
2017-10-02 19:44:17 -0700

    New block and hash

--- blockchain.py
+++ blockchain.py
@@ -15,17 +15,35 @@
 #     'proof': 324984774000,
 #     'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
 # }
+import hashlib
+import json
+from time import time
 
 class Blockchain(object):
     def _init_(self):
         self.chain = []
         self.current_transactions = []
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
 
     # Create a new block and add it to the chain
-    def new_block(self):
-        pass
+    # proof: <int> The proof given by the Proof of Work algorithm
+    # previous_hash: <str> Hash of previous Block
+    # return: <dict> New Block
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+        # Reset the current list of transactions
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
-    # Add a new transaction to the list of transactions
+    # Add a new transaction to the list of transactions.
     # Creates a new transaction to go into the next mined block.
     # sender: <str> Address of the sender.
     # recipient: <str> Address of the recipient.
@@ -40,11 +58,16 @@ class Blockchain(object):
         return self.last_block['index'] + 1
 
     # Hash a block
+    # We must make sure that the dict is ordered, or we'll have inconsistent hashes
+    # Creates a SHA-256 hash of a Block
+    # block: <dict> Block
+    # return: <str>
     @staticmethod
     def hash(block):
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     # Return the last block in the chain
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
